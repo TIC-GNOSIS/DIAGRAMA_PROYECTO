12c0ef55c646055293a486d109978f420dd959ce
IngJuanMaSuarez <ingjuanmasuarez@gmail.com>
2022-08-27 09:38:21 -0500

    Uso de h2database y configuracion de la misma

--- src/main/java/com/juanma/kikeana/entity/Employee.java
+++ src/main/java/com/juanma/kikeana/entity/Employee.java
@@ -24,7 +24,8 @@ public class Employee {
     @JoinColumn(name = "profile_id")
     private Profile profile;
 
-    @OneToMany
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
     private Enum_RolName role;
 
     @ManyToOne
@@ -41,95 +42,4 @@ public class Employee {
     @Column(name = "updatedAt")
     private Date updatedAt;
 
-    public Employee() {
-    }
-
-    public Employee(Long id, String email, Profile profile, Enum_RolName role, Enterprise enterprise, List<Transaction> transactions, Date createdAt, Date updatedAt) {
-        this.id = id;
-        this.email = email;
-        this.profile = profile;
-        this.role = role;
-        this.enterprise = enterprise;
-        this.transactions = transactions;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Profile getProfile() {
-        return profile;
-    }
-
-    public void setProfile(Profile profile) {
-        this.profile = profile;
-    }
-
-    public Enum_RolName getRole() {
-        return role;
-    }
-
-    public void setRole(Enum_RolName role) {
-        this.role = role;
-    }
-
-    public Enterprise getEnterprise() {
-        return enterprise;
-    }
-
-    public void setEnterprise(Enterprise enterprise) {
-        this.enterprise = enterprise;
-    }
-
-    public List<Transaction> getTransactions() {
-        return transactions;
-    }
-
-    public void setTransactions(List<Transaction> transactions) {
-        this.transactions = transactions;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", profile=" + profile +
-                ", role=" + role +
-                ", enterprise=" + enterprise +
-                ", transactions=" + transactions +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
-    }
 }
