7d17eb8e8a5fbe2e945be084a203fd2bec3c7f0d
IngJuanMaSuarez <ingjuanmasuarez@gmail.com>
2022-08-27 11:07:39 -0500

    Se muestra via web on un get el resultado de un set para probar

--- src/main/java/com/juanma/kikeana/KikeanaApplication.java
+++ src/main/java/com/juanma/kikeana/KikeanaApplication.java
@@ -1,29 +1,13 @@
 package com.juanma.kikeana;
 
-import com.juanma.kikeana.entity.Enterprise;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KikeanaApplication implements CommandLineRunner {
-
-	private Enterprise enterprise;
-
-	@Autowired
-	public KikeanaApplication(Enterprise enterprise) {
-		this.enterprise = enterprise;
-	}
+public class KikeanaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KikeanaApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		enterprise.setAddress("Calle falsa numero 123");
-		System.out.println(enterprise.getAddress());
 	}
 
 }
